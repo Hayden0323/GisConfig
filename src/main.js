1b9d5837ae5347581e320116dd3779b884db10cf
Hayden <haosong0323@gmail.com>
2020-08-12 14:42:34 +0800

    chore: add vuetify

--- src/main.js
+++ src/main.js
@@ -1,21 +1,17 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from '@/plugins/vuetify'
 
-//引入element
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
+import 'normalize.css'
+import './assets/style/common.less'
 
-import 'normalize.css'; 
-import './assets/style/common.less';
- 
-
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
   router,
   store,
-  render: (h) => h(App)
-}).$mount('#app');
+  render: (h) => h(App),
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
-// vue.config.js 配置说明 
+// vue.config.js 配置说明
 // 这里只列一部分，具体配置参考官方文档https://cli.vuejs.org/zh/config/#css-loaderoptions
 
-const path = require("path");
-const webpack = require("webpack");
-const CompressionPlugin = require("compression-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path')
+const webpack = require('webpack')
+const CompressionPlugin = require('compression-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-const cesiumSource = "node_modules/cesium/Source";
-const cesiumWorkers = "../Build/Cesium/Workers";
+const cesiumSource = 'node_modules/cesium/Source'
+const cesiumWorkers = '../Build/Cesium/Workers'
 
 module.exports = {
-  publicPath: "/",
+  publicPath: '/',
 
   //是否使用eslint
   lintOnSave: false,
@@ -25,73 +25,89 @@ module.exports = {
 
   // 它支持webPack-dev-server的所有选项
   devServer: {
-    host: "localhost", //也可以直接写IP地址这样方便真机测试
+    host: '192.168.0.172', //也可以直接写IP地址这样方便真机测试
     port: 3001, // 端口号
     https: false, // https:{type:Boolean}
-    open: true, //配置自动启动浏览器
+    open: false, //配置自动启动浏览器
   },
 
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.node
-      .set("fs", "empty")
+      .set('fs', 'empty')
       .end()
-      .resolve.alias.set("cesium", path.resolve(__dirname, cesiumSource))
-      .set("mars-map", path.resolve(__dirname, "./src/components/mars-map"))
-      .set("mars-ui", path.resolve(__dirname, "./src/components/mars-ui"))
+      .resolve.alias.set('cesium', path.resolve(__dirname, cesiumSource))
+      .set('mars-map', path.resolve(__dirname, './src/components/mars-map'))
+      .set('mars-ui', path.resolve(__dirname, './src/components/mars-ui'))
       .end()
       .end()
       .amd({
-        toUrlUndefined: true
+        toUrlUndefined: true,
       })
-      .module.set("unknownContextCritical", false)
+      .module.set('unknownContextCritical', false)
       .rule()
       .include.add(path.resolve(__dirname, cesiumSource))
       .end()
       .post()
       .pre()
       .test(/\.js$/)
-      .use("strip")
-      .loader("strip-pragma-loader")
+      .use('strip')
+      .loader('strip-pragma-loader')
       .options({
         pragmas: {
-          debug: false
-        }
+          debug: false,
+        },
       })
       .end()
-      .end();
+      .end()
   },
 
   configureWebpack: () => {
-    if (process.env.NODE_ENV === "production") {
+    if (process.env.NODE_ENV === 'production') {
       return {
         plugins: [
           new CompressionPlugin({
             test: /\.js$|\.html$|\.css/,
             threshold: 10240,
-            deleteOriginalAssets: false
+            deleteOriginalAssets: false,
           }),
-          new webpack.DefinePlugin({ CESIUM_BASE_URL: JSON.stringify("static") }),
-          new CopyWebpackPlugin([{ from: path.join(cesiumSource, "Assets"), to: "static/Assets" }]),
-          new CopyWebpackPlugin([{ from: path.join(cesiumSource, "ThirdParty"), to: "static/ThirdParty" }]),
-          new CopyWebpackPlugin([{ from: path.join(cesiumSource, "Widgets"), to: "static/Widgets" }]),
-          new CopyWebpackPlugin([{ from: path.join(cesiumSource, cesiumWorkers), to: "static/Workers" }])
+          new webpack.DefinePlugin({
+            CESIUM_BASE_URL: JSON.stringify('static'),
+          }),
+          new CopyWebpackPlugin([
+            { from: path.join(cesiumSource, 'Assets'), to: 'static/Assets' },
+          ]),
+          new CopyWebpackPlugin([
+            {
+              from: path.join(cesiumSource, 'ThirdParty'),
+              to: 'static/ThirdParty',
+            },
+          ]),
+          new CopyWebpackPlugin([
+            { from: path.join(cesiumSource, 'Widgets'), to: 'static/Widgets' },
+          ]),
+          new CopyWebpackPlugin([
+            {
+              from: path.join(cesiumSource, cesiumWorkers),
+              to: 'static/Workers',
+            },
+          ]),
           // new BundleAnalyzerPlugin()
-        ]
-      };
+        ],
+      }
     } else {
       return {
-        devtool: "source-map",
+        devtool: 'source-map',
         plugins: [
-          new webpack.DefinePlugin({ CESIUM_BASE_URL: JSON.stringify("") }),
+          new webpack.DefinePlugin({ CESIUM_BASE_URL: JSON.stringify('') }),
           new CopyWebpackPlugin([
-            { from: path.join("./public", "data"), to: "data" },
-            { from: path.join(cesiumSource, "Assets"), to: "Assets" },
-            { from: path.join(cesiumSource, "ThirdParty"), to: "ThirdParty" },
-            { from: path.join(cesiumSource, "Widgets"), to: "Widgets" },
-            { from: path.join(cesiumSource, cesiumWorkers), to: "Workers" }
-          ])
-        ]
-      };
+            { from: path.join('./public', 'data'), to: 'data' },
+            { from: path.join(cesiumSource, 'Assets'), to: 'Assets' },
+            { from: path.join(cesiumSource, 'ThirdParty'), to: 'ThirdParty' },
+            { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' },
+            { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' },
+          ]),
+        ],
+      }
     }
-  }
-};
+  },
+}
